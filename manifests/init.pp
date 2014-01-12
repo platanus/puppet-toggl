8426f7cb9db224000125733735cc1e26e5c80ee0
Jonathan Webb <webbj2012@jwwebb.info>
2014-01-12 14:20:18 -0500

    Move comment out of resource declaration to quiet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,10 @@
 # Examples
 #
 #   include toggl
+#
+# TODO: switch source to the following, pending release of puppetlabs/puppet#2249
+#   source => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
+#
 class toggl {
   file {'/var/db/.puppet_appdmg_installed_Toggl':
     ensure => 'absent',
@@ -10,8 +14,6 @@ class toggl {
   }
   package { 'Toggl':
     provider => 'appdmg',
-    # TODO: switch source to the following, pending release of puppetlabs/puppet#2249
-    # source => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
     source => 'https://55aa37393ef0d1f9fb76-e65c36a55026053efe5868c3826f43f1.ssl.cf1.rackcdn.com/TogglDesktop_2013-10-17-13-05-57.dmg'
   }
 }
