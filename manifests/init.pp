77d8b71439ad68688b16e68e75ebb624f079d6d9
Jonathan Webb <webbj2012@jwwebb.info>
2014-01-12 14:26:56 -0500

    Fixing actual lint issue.

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,7 +5,7 @@
 #   include toggl
 #
 # TODO: switch source to the following, pending release of puppetlabs/puppet#2249
-#   source => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
+#   source   => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
 #
 class toggl {
   file {'/var/db/.puppet_appdmg_installed_Toggl':
@@ -14,6 +14,6 @@ class toggl {
   }
   package { 'Toggl':
     provider => 'appdmg',
-    source => 'https://55aa37393ef0d1f9fb76-e65c36a55026053efe5868c3826f43f1.ssl.cf1.rackcdn.com/TogglDesktop_2013-10-17-13-05-57.dmg'
+    source   => 'https://55aa37393ef0d1f9fb76-e65c36a55026053efe5868c3826f43f1.ssl.cf1.rackcdn.com/TogglDesktop_2013-10-17-13-05-57.dmg'
   }
 }
