a1fd8ff938e7bef89b90f39ece0ae1407efba528
Jonathan Webb <webbj2012@jwwebb.info>
2014-01-12 12:42:13 -0500

    Updates manifest to remove the appdmg 'cookie' and download the latest installer

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,8 +4,14 @@
 #
 #   include toggl
 class toggl {
+  file {'/var/db/.puppet_appdmg_installed_Toggl':
+    ensure => 'absent',
+    before => Package['Toggl']
+  }
   package { 'Toggl':
     provider => 'appdmg',
-    source   => 'https://download.toggl.com/toggldesktop/latest/TogglDesktopInstaller_Release.dmg'
+    # TODO: switch source to the following, pending release of puppetlabs/puppet#2249
+    # source => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
+    source => 'https://55aa37393ef0d1f9fb76-e65c36a55026053efe5868c3826f43f1.ssl.cf1.rackcdn.com/TogglDesktop_2013-10-17-13-05-57.dmg'
   }
 }
