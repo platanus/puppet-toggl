7e92a2260e9ff8d44037de5aa4a823017a1f46ca
Jonathan Webb <webbj2012@jwwebb.info>
2014-01-13 22:38:39 -0500

    Only delete the appdmg cookie if it contains old value

--- manifests/init.pp
+++ manifests/init.pp
@@ -8,9 +8,12 @@
 #   source   => 'https://www.toggl.com/api/v8/installer?app=td&platform=darwin&channel=stable'
 #
 class toggl {
-  file {'/var/db/.puppet_appdmg_installed_Toggl':
-    ensure => 'absent',
-    before => Package['Toggl']
+  if file('/var/db/.puppet_appdmg_installed_Toggl', '/dev/null') =~ /https:\/\/download\.toggl\.com\/toggldesktop\/latest\/TogglDesktopInstaller_Release\.dmg/ {
+    notice('Detected an old version of puppet-toggl (1.0.2 or older). The appdmg cookie will be removed so that the latest version of ToggleDesktop.app can be installed.')
+    file {'/var/db/.puppet_appdmg_installed_Toggl':
+      ensure => 'absent',
+      before => Package['Toggl']
+    }
   }
   package { 'Toggl':
     provider => 'appdmg',
