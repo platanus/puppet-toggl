a16e295edad0d083071dc8843835013fcb01eae9
Juan Ignacio Donoso <jidonoso@gmail.com>
2014-06-09 10:31:24 -0400

    fix tests

--- spec/classes/toggl_spec.rb
+++ spec/classes/toggl_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 
 describe 'toggl' do
   it do
+    should contain_class('toggl')
     should contain_package('Toggl').with({
       :provider => 'appdmg',
       :source   => 'http://assets.toggl.com/installers/TogglDesktop-7_877-2014-06-09-12-59-14.dmg',
